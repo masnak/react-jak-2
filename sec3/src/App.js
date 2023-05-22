a3a11e4fd02edff677913b7a3faf09fb96d8ee49
masnak <masamichi.nakamura@gmail.com>
2023-05-22 11:13:13 +0900

    sec3

--- sec3/src/App.js
+++ sec3/src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import { ChildArea } from "./ChildArea";
 
 function App() {
   console.log("App");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
+
   return (
     <div className="App">
       <h1>test</h1>
+      <input value={text} onChage={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
 }
