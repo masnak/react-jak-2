2a09c2b67962a3278b738759a105d5ed83354b93
masnak <masamichi.nakamura@gmail.com>
2023-05-26 09:47:54 +0900

    primary button

--- sec6/src/App.js
+++ sec6/src/App.js
@@ -1,8 +1,11 @@
+import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
+
 function App() {
   return (
     <div className="App">
       <h1>Test</h1>
       <p>react</p>
+      <PrimaryButton>テスト</PrimaryButton>
     </div>
   );
 }
